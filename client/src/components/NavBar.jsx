c97a27c082758e518525e7a38fa8ea9eaa716873
Geoffrey PECHIN <geoffrey.pechin@gmail.com>
2024-09-15 14:47:02 +0200

    :art: added picture , navbar done desktop/mobile

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,8 +1,32 @@
+import MiniStanLee from "../assets/images/Mini_stan_lee.png";
+import MenuBurger from "../assets/images/MenuBurger_infinity_Roster.png";
+
 export default function NavBar() {
   return (
-    <div className="NavBar">
-      <h1>Infinity Roster</h1>
-      <p>Hero</p>
+    <div>
+      <div className="NavBar_mobile">
+        <img src={MiniStanLee} alt="figurine de stan lee " />
+        <h1>Infinity Roster</h1>
+        <button className="NavBar_menu" type="button">
+          <img src={MenuBurger} alt="trois trait pour ouvrir le menu burger " />{" "}
+        </button>
+      </div>
+      <div className="NavBar_desktop">
+        <div className="NavBar_Stan_title">
+          <img src={MiniStanLee} alt="figurine de stan lee " />
+          <h1>Infinity Roster</h1>
+        </div>
+        <div className="NavBar_menu_list">
+          <ul>
+            <li>Accueil</li>
+            <li>Hero</li>
+            <li>Vilain</li>
+            <li>Création de compte</li>
+            <li>Connexion</li>
+            <li>Favoris</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,8 +1,15 @@
 export default function HomePage() {
   return (
-    <div className="Homepage">
-      <h1>Infinity Roaster</h1>
-      <p>Marvel team's</p>
+    <div className="Homepage_list">
+      <ul>
+        <li>
+          <button type="button">Création de compte</button>
+        </li>
+        <li>Connexion</li>
+        <li>Hero</li>
+        <li>Vilain</li>
+        <li>Favoris</li>
+      </ul>
     </div>
   );
 }
