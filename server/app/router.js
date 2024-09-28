966ca283b409bc15d77b585335e359c603d452b8
Geoffrey PECHIN <geoffrey.pechin@gmail.com>
2024-09-28 12:44:11 +0200

    :bug: fix bug multer

--- server/app/router.js
+++ server/app/router.js
@@ -4,16 +4,17 @@ const router = express.Router();
 
 const heroActions = require("./controllers/heroActions");
 const bandActions = require("./controllers/bandActions");
+const uploadPicture = require("./services/middlewar");
 
 router.get("/hero", heroActions.browse);
 router.get("/band", bandActions.browse);
 
 router.get("/hero/:id", heroActions.read);
 
-router.post("/hero", heroActions.add);
+router.post("/hero", uploadPicture, heroActions.add);
 
 router.delete("/hero/:id", heroActions.destroy);
 
-router.put("/hero/:id", heroActions.edit);
+router.put("/hero/:id", uploadPicture, heroActions.edit);
 
 module.exports = router;
